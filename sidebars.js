92ee9cfce641ad15318df44a7076316250a13834
Watchmaker <rajk@opentensor.dev>
2024-11-04 15:42:49 -0800

    Ongoing updates 4

--- sidebars.js
+++ sidebars.js
@@ -84,6 +84,16 @@ const sidebars = {
     },
     {
       type: "category",
+      label: "Dynamic TAO",
+      link: {type: "doc", id: "dynamic-tao/index",},
+      collapsible: true,
+      collapsed: false,
+      items: [
+        "dynamic-tao/dtao-btcli",
+      ],
+    },
+    {
+      type: "category",
       label: "Staking and Delegation",
       collapsible: true,
       collapsed: false,
@@ -131,7 +141,6 @@ const sidebars = {
         "evm-tutorials/ed25519-verify-precompile",
       ],
     },
-    "dtao-btcli",
     "btcli",
     "bt-api-ref",
     "emissions",
